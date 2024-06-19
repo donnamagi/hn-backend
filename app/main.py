40b868c455d4e93497ce1ac5380e61bc70e48f03
Donna Mägi <magi.donna@gmail.com>
2024-06-19 09:38:47 +0300

    including sentry

--- app/main.py
+++ app/main.py
@@ -3,6 +3,21 @@ from app.routers import jobs, articles, keywords
 from app.dependencies import DatabaseService
 from app.services.Scheduler import SchedulerService
 from contextlib import asynccontextmanager
+import sentry_sdk
+import os
+
+
+""" Sentry setup to monitor errors in the live app. https://hacker-news.sentry.io/ """
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 
 
 """ This code will be executed once, before the application starts (and stops) receiving requests """
@@ -26,3 +41,7 @@ app.include_router(jobs.router)
 @app.get("/")
 async def root():
   return {"message": "Hello World"}
+
+@app.get("/sentry-debug")
+async def trigger_error():
+  division_by_zero = 1 / 0
