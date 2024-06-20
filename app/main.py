e5b10bd928c9e64d6d3392faae92eefb6e50cb61
Donna Mägi <magi.donna@gmail.com>
2024-06-20 14:27:45 +0300

    cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import jobs, articles, keywords
 from app.dependencies import DatabaseService
 from app.services.Scheduler import SchedulerService
@@ -34,6 +35,22 @@ async def lifespan(app: FastAPI):
   await scheduler.stop()
 
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost:3000",
+    "https://hackernews.news",
+    "https://www.hackernews.news",
+    "https://hn-front.vercel.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(articles.router)
 app.include_router(keywords.router)
 app.include_router(jobs.router)
