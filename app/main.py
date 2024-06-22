c3a01107a3e739c9be1ba2681a9714854fd1c51c
Donna Mägi <magi.donna@gmail.com>
2024-06-22 11:48:04 +0300

    updated logging setup

--- app/main.py
+++ app/main.py
@@ -6,20 +6,34 @@ from app.services.Scheduler import SchedulerService
 from contextlib import asynccontextmanager
 import sentry_sdk
 import os
+from sentry_sdk.integrations.logging import LoggingIntegration
+import logging
 
 
-""" Sentry setup to monitor errors in the live app. https://hacker-news.sentry.io/ """
-sentry_sdk.init(
-    dsn=os.getenv("SENTRY_DSN"),
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
+""" Sentry setup to monitor errors in the live app. https://donnamagi.sentry.io/ """
+def setup_logging():
+  logging.basicConfig(level=logging.INFO,
+                      format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                      datefmt='%Y-%m-%d %H:%M:%S')
+  
+  print("Logging configured")
+
+sentry_logging = LoggingIntegration(
+  level=logging.INFO,
+  event_level=logging.ERROR 
 )
 
+sentry_sdk.init(
+  dsn=os.getenv("SENTRY_DSN"),
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  traces_sample_rate=1.0,
+  # Set profiles_sample_rate to 1.0 to profile 100%
+  # of sampled transactions.
+  # We recommend adjusting this value in production.
+  profiles_sample_rate=1.0,
+  integrations=[sentry_logging]
+)
 
 """ This code will be executed once, before the application starts (and stops) receiving requests """
 @asynccontextmanager
@@ -27,6 +41,8 @@ async def lifespan(app: FastAPI):
   # on startup
   db_service = DatabaseService()
   scheduler = SchedulerService()
+  setup_logging()
+
 
   yield 
 
