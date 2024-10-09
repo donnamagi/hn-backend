95188fec37fa74f55f018b4ef8f23e6478536b0d
Donna Mägi <magi.donna@gmail.com>
2024-10-09 18:14:10 +0200

    logging

--- app/handlers/webhooks.py
+++ app/handlers/webhooks.py
@@ -1,5 +1,10 @@
 from app.services.whatsapp import WhatsAppService
 from app.services.messages import MessageService
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class WebhookHandler:
@@ -26,10 +31,14 @@ class WebhookHandler:
             response = self.message_service.create_response(
                 message["type"], message["text"]
             )
+            logging.info(f"Created response: {response}")
 
             await self.whatsapp_service.send_message(
                 business_phone_number_id, message["from"], response, message["id"]
             )
+            logging.info(f"Sent response: {message['id']}")
+
             await self.whatsapp_service.mark_as_read(
                 business_phone_number_id, message["id"]
             )
+            logging.info(f"Message marked as read: {message['id']}")
